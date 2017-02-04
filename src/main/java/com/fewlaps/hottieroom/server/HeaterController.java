041fbd18bc5b294f0191a1b08763c98b6db7bb81
Roc Boronat <roc@fewlaps.com>
2017-02-05 00:54:44 +0100

    Set a produces text/plain modifier to send plain responses, to help Arduino’s Ciao library with the parsing

--- src/main/java/com/fewlaps/hottieroom/server/HeaterController.java
+++ src/main/java/com/fewlaps/hottieroom/server/HeaterController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HeaterController {
 
-    @RequestMapping("/heater")
+    @RequestMapping(value = "/heater", produces = "text/plain")
     public String heater(@RequestParam(value = "temperature") String temperature) {
         Application.repository.setActualTemperature(Double.valueOf(temperature));
         return "" + Application.repository.getDesiredTemperature();
